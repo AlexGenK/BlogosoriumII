f2d2e53f55e4a767d79a954f9231f042374862f2
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-05-16 16:50:12 +0300

    add posts

--- app.rb
+++ app.rb
@@ -16,6 +16,9 @@ end
 
 # главная страница
 get '/' do
+	
+	# выборка простов в порядке обратном порядку создания
+	@p=Post.order(:updated_at).reverse_order
 	erb :posts
 end
 
@@ -32,18 +35,16 @@ end
 
 # обработчик формы создания поста
 post '/new' do
-  @name=params[:name]
-  @post=params[:post]
 
-  # получение сообщения об ошибке. если есть, то снова вводим,
-  # если нет, то пост записывается в БД и идет перенаправление на главную страницу
-  @error=get_error_message({:name=>"Enter your name. ", :post=>"Enter your post"})
-  if @error==""
-  	$db.execute("INSERT INTO posts (p_name, p_post) VALUES (?, ?)", [@name, @post])
-  	redirect to ('/')
-  else
-  	erb :new
-  end
+	p=Post.new params[:post]
+
+	# получение сообщения об ошибке. если есть, то снова вводим,
+	# если нет, то пост записывается в БД и идет перенаправление на главную страницу
+	if p.save
+		redirect to ('/')
+	else
+		erb :new
+	end
 end
 
 # обработчик формы ввода комментария
@@ -54,17 +55,8 @@ post '/post/:id' do
 
   	# получение сообщения об ошибке. если нет, то комментарий записывается в БД и 
   	# в любом случае идет перенаправление на форму с детальной информацией о посте
-  	@error=get_error_message({:name_c=>"Enter your name. ", :comment=>"Enter your comment"})
+
  	if @error==""
-  		$db.execute("INSERT INTO comments (p_id, c_name, c_comment) VALUES (?, ?, ?)", [@id, @name_c, @comment])
   	end
   	erb :onepost
-end
-
-# возвращает сообщение о возможных ошибках. принмимает хеш с парой
-# имя_параметра=>выводимое сообщение. если параметр пустой, формируется сообщение
-def get_error_message(hh)
-	err=""
-	hh.each_key {|param| err+=hh[param] if params[param].strip==""}
-	return err
 end
